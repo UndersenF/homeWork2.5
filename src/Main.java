e581cfeff6c680a58c8032c717f760afcc1a4e62
UndersenF <begintolife@mail.ru>
2024-08-06 21:24:16 +0300

    Hw is done.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import exception.WrongLoginException;
+import exception.WrongPasswordException;
+import service.ValidationService;
+
+import static service.ValidationService.validate;
+
 public class Main {
     public static void main(String[] args) {
-
+        try {
+            validate("login", "password", "password");
+            System.out.println("Ооо все окей!");
+        } catch (WrongPasswordException a) {
+            System.out.println("С паролем что-то не так!");
+        } catch (WrongLoginException b) {
+            System.out.println("С логином что-то не так!");
+        }
+//todo Найти как прекратить выполнение блока final
     }
 }
